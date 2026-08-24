eaed9c8c6e74046b18a4cc5d56f58d8ff779985f
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2026-08-24 14:48:58 +0100

    add server only dependency

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unsplash.com",
+        hostname: "image.tmdb.org",
       },
     ],
     formats: ["image/avif", "image/webp"],
